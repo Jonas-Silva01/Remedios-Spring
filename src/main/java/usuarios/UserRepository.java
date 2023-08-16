c81429e596c13887a94bcd9bebcf1816dc0f0596
Jonas <jonasmendocasilva@gmail.com>
2023-08-15 22:35:34 -0400

    Adição do UserRepository e inicio da autenticação do Usuário

---
+++ src/main/java/usuarios/UserRepository.java
@@ -0,0 +1,10 @@
+package usuarios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<Usuario, Long> {
+
+	UserDetails findByLogin(String login);
+
+}
